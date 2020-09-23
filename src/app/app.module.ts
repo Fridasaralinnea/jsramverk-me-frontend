85e3c213325ccdf81e3319fedbde53d31e35d594
Frida Larsson <fridasaralinnea@gmail.com>
2020-09-23 09:04:34 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Custom components
+import { HomeComponent } from './home/home.component';
+import { ReportComponent } from './reports/reports.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    // { path: 'reports', component: ReportComponent, pathMatch: 'prefix' },
+    // { path: 'reports/week', component: ReportComponent, pathMatch: 'full' },
+    { path: 'reports/week/1', component: ReportComponent, pathMatch: 'full' },
+    // { path: '**', redirectTo: '/', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReportComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/reports/reports.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+    selector: 'app-reports',
+    templateUrl: './reports.component.html',
+    styleUrls: ['./reports.component.css']
+})
+export class ReportComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+}
